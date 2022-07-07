b63c9e161c6630a7e399e8b5849338a1427b260f
Himaff <marc_affechi@hotmail.com>
2022-07-06 17:06:17 -0700

    Functions & Nested Loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main() {
+	int i = 0;
+	char name_of_school[] = "_putchar";
+
+	while (i < 9)
+	{
+		_putchar(name_of_school[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
