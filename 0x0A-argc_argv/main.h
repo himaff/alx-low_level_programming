fa05be1d3da1d2541f73df873e02c37c9563d7dc
Himaff <marc_affechi@hotmail.com>
2022-07-27 19:31:50 -0700

    0x0A argc argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+int _putchar(char c);
+void print_string(char *str);
+void print_number(int num);
+void print_array_elements(char *str);
+int _atoi(char *s);
+int multiply_two_nums(int num1, int num2);
+void print_number(int n);
